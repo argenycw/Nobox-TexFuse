abce5b275a0b3c063e3ac0f0e6f2c5ad3dd217aa
argenycw <yanwaiwai133@gmail.com>
2021-04-14 03:05:33 +0800

    large update (see the log for more info)

--- readme.md
+++ readme.md
@@ -9,6 +9,15 @@ This program runs in Python3 and requires the following pip modules:
 - torchvision==0.4.1
 - Pillow==8.0.1
 
+## Models
+
+In current version, the script supports two models pretrained on VGGFace2.
+- VGG16 (Default)  `--model vgg` 
+- ResNet50 `--model resnet`
+
+You can download the .pth file [here](https://drive.google.com/file/d/1fuNhlJ2X36zXqh6Ab8A253Xe6ztDJZ86/view?usp=sharing). Unzip the files in `pretrained/` folder so that the pretrained weights are stored as `pretrained/resnet50_scratch_dag.pth` and `pretrained/vgg_face_dag.pth` respectively. **Make sure you have the two weights downloaded properly before running the script.**
+
+Since the pretrained weights of the two models are different, it is normal to see different similarity scores with different pretrained models.
 
 ## Usage
 
@@ -19,10 +28,11 @@ python texfuse.py --source <source image> --target <target image>
 
 The output image will be saved as `outputs/attack.png` by default. You can overwrite the path with the `-o` or `--out` argument.
 
-The script automatically resizes the picture into dimension 224x224. To disable such feature, use the `--noresize` argument:
+The script automatically resizes the picture into dimension 224x224. To disable such feature, you can consider upsampling the perturbation with the argument `-u` or `--upsample`. With `-u` enabled, the perturbation will be upsampled, forming a more apparent "noise" on top of the source image.
+
 ```
-python texfuse.py --source <source image> --target <target image> --out <output image> --noresize
+python texfuse.py --source <source image> --target <target image> --out <output image> -u
 ```
-Alternatively, you can also consider upsampling the perturbation with the argument `-u` or `--upsample`.
+
 
 Batch computation and GPU are currently not supported.
--- texfuse.py
+++ texfuse.py
@@ -14,10 +14,14 @@ import numpy as np
 from PIL import Image
 import torch
 import torch.nn.functional as F
-import torchvision.models as models
+#import torchvision.models as models
+from model import *
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torch.autograd.gradcheck import zero_gradients
+
+import warnings
+warnings.filterwarnings("ignore") # suppress UserWarnings
 
 def capture_feature(module, input, output):
     global intermediate_features
@@ -31,23 +35,42 @@ def vgg16_manipulate(vgg16):
     vgg16.classifier = torch.nn.Identity()
     return vgg16
 
+# [unknown, unknown] => [0, 1]
+def unnormalize(img, mean, std):
+    img[0] = img[0] * std[0] + mean[0]
+    img[1] = img[1] * std[1] + mean[1]
+    img[2] = img[2] * std[2] + mean[2]
+    if std == [1, 1, 1]:
+        img /= 255.0
+    return img
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='FaceFuse by AG')
+    parser.add_argument('--model', default='vgg', type=str, help='The pretrained model to pick from (vgg/resnet)') 
     parser.add_argument('--source', default='', type=str, help='the path of the source image')
     parser.add_argument('--target', default='', type=str, help='the path of the target image') 
-    parser.add_argument('--eps', default=50, type=int, help='Maximum perturbation strength (over 255)') 
+    parser.add_argument('--eps', default=10, type=int, help='Maximum perturbation strength (over 255)') 
     parser.add_argument('--iter', default=50, type=int, help='Number of iterations for projected gradient computation') 
-    parser.add_argument('--step_size', default=5, type=int, help='Please just keep it as default, thanks.') 
+    parser.add_argument('--step_size', default=2, type=int, help='Please just keep it as default, thanks.') 
     parser.add_argument('-o', '--out', default='attack.png', type=str, help='The output filename for the attacked image.') 
     parser.add_argument('-u', '--upsample', action='store_true', help='Upsample the perturbation rather than doublesample the output.')
-    parser.add_argument('-n', '--noresize', action='store_true', help='Prohibit the script from resizing the images (also accept rectanuglar shape)') 
     args = parser.parse_args()
 
-    # load a pretrained VGG16 model on ImageNet 
-    # actually I am looking for models pretrained on face dataset instead :((((
-    vgg16 = models.vgg16(pretrained=True) # TODO going to update this
-    vgg16 = vgg16_manipulate(vgg16)
-    #vgg16.features[15].register_forward_hook(capture_feature)
+    # load a pretrained models from VGGFace or VGGFace2
+    try:
+        if args.model == 'vgg':
+            net = vgg_face_dag(weights_path="pretrained/vgg_face_dag.pth")
+            meta = net.meta
+        elif args.model == 'resnet':
+            net = resnet50_scratch_dag(weights_path="pretrained/resnet50_scratch_dag.pth")
+            meta = net.meta
+        else:
+            print('Undefined model type: %s' % args.model)
+            exit(0)
+    except FileNotFoundError as err:
+        print("File not found: ", err)
+        print("Please make sure you have downloaded the pretrained models specified in readme.md")
+        exit(0)
 
     # input validation
     if args.source == '' or args.target == '':
@@ -59,52 +82,38 @@ if __name__ == '__main__':
     
     # load sample images
     source_img_path = args.source
-    target_img_path = args.target
-
-    img_preprocess = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),    
-    ])
-    unnormalize = transforms.Normalize(
-        mean= [-0.485/0.229, -0.456/0.224, -0.406/0.255],
-        std= [1/0.229, 1/0.224, 1/0.255]
-    )
+    target_img_path = args.target    
 
     source_raw = Image.open(source_img_path).convert("RGB")
     target_raw = Image.open(target_img_path).convert("RGB")
     source_w, source_h = source_raw.size
-    target_w, target_h = target_raw.size    
+    target_w, target_h = target_raw.size
+
+    img_preprocess = compose_transforms(meta)
 
     if abs(1 - ((source_w / target_w) / (source_h / target_h))) > 0.1:
         print('Warning: A huge difference between dimensions in source and target is detected. Note the image may be stretched to a certain degree.')
-    if args.noresize:
-        if source_w != target_w or source_h != target_h:
-            print('Resizing target (%d, %d) into source (%d, %d).' % (target_w, target_h, source_w, source_h))            
-        source_img = source_raw
-        target_img = target_raw.resize((source_w, source_h))
-    else:
-        source_img = source_raw.resize((224, 224))
-        target_img = target_raw.resize((224, 224))
+    if (source_w != source_h):
+        print('Warning: Non-square source image input. The final output may possibly be stretched.')
 
-    source_img = img_preprocess(source_img).unsqueeze(0)
-    target_img = img_preprocess(target_img).unsqueeze(0)
+    source_img = img_preprocess(source_raw).unsqueeze(0)
+    target_img = img_preprocess(target_raw).unsqueeze(0)
 
     # to capture the intial features
-    source_feature = vgg16(source_img)
-    target_feature = vgg16(target_img)
+    source_feature = net(source_img)
+    target_feature = net(target_img)
 
     num_iter = args.iter
-    eps = args.step_size / 255.0
-    max_eps = args.eps / 255.0
-    #source_feature = intermediate_features[0]
-    #target_feature = intermediate_features[1]
+    scale = source_img.max() - source_img.min()
+    eps = args.step_size / 255.0 * scale
+    max_eps = args.eps / 255.0 * scale
+
     adv_img = source_img.clone() + torch.zeros_like(source_img).uniform_(-eps, eps)
     for i in range(num_iter):
         adv_img.requires_grad_()
         zero_gradients(adv_img)
 
-        feature = vgg16(adv_img)
-        #feature = intermediate_features[-1]
+        feature = net(adv_img)
 
         loss = F.l1_loss(feature, target_feature)
         loss.backward(retain_graph=True)
@@ -114,13 +123,19 @@ if __name__ == '__main__':
         adv_img = torch.min(torch.max(adv_img, source_img - max_eps), source_img + max_eps)
         # log progress
         print('Iter: %d / %d. Feature Similarity: %f' % (i+1, num_iter, loss.item()), end='\r')        
+    print()
     
     # upsample the perturbation when set
     if args.upsample:
+        '''
         delta = adv_img - source_img
         delta_large = F.interpolate(delta, size=(source_h, source_w), mode='bicubic', align_corners=False)
-        adv_img = img_preprocess(source_raw) + delta_large
+        uptemptransform = compose_transforms(meta, to_resize=False)
+        adv_img = uptemptransform(source_raw) + delta_large
+        '''
+        print('Upsample: (%d, %d) => (%d, %d)' % (adv_img.shape[-1], adv_img.shape[-2], source_w, source_h))
+        adv_img = F.interpolate(adv_img, size=(source_h, source_w), mode='bicubic', align_corners=False)
 
     outfile = join('output', args.out)
-    save_image(unnormalize(adv_img.squeeze(0)), outfile)
+    save_image(unnormalize(adv_img.squeeze(0), meta['mean'], meta['std']), outfile)
     print("Attack Completed. Example is saved as %s" % outfile)
